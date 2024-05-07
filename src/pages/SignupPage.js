6650336cea674bae68a31143a1d3c616e61ea20c
Dhruvang Choudhari <dhruvangsc@gmail.com>
2024-05-07 23:56:01 +0530

    Home Login and Signup without Backend Done

---
+++ src/pages/SignupPage.js
@@ -0,0 +1,14 @@
+import Signup from "../features/auth/components/SignUp";
+
+
+
+function SignupPage() {
+    return ( 
+        <div>
+           <Signup></Signup>
+        </div>
+     );
+}
+
+export default SignupPage;
+
